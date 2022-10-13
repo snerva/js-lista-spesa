63d7b24d24a519214ef7e35376a3e972864ccf3a
snerva <selenenerva@live.it>
2022-10-13 15:41:00 +0200

    dom shopping list

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,12 @@ const shoppingList = [
     'Pizza'
 ];
 
+const listEl = document.querySelector('.shopping_list');
 let i = 0;
 while (i<shoppingList.length){
     const listElement = shoppingList[i];
     console.log(listElement);
-    const listItem = `<li>${shoppingList[i]}</li>`;
-    document.querySelector('ul').innerHTML = `${listItem}`;
+    const listItemMarkup = `<li>${shoppingList[i]}</li>`;
+    listEl.insertAdjacentHTML('beforeend', listItemMarkup);
     i++
 }
